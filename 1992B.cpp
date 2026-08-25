25b4b0927a7ecb2fed7f59edfab5eb4ab9f4417c
tiwariankit1708 <6295801ankit@gmail.com>
2026-08-25 22:47:52 +0530

    1992B done

---
+++ 1992B.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void solve() {
+    long long n;
+    int k;
+    cin >> n >> k;
+    
+    long long max_val = 0;
+    for (int i = 0; i < k; ++i) {
+        long long a;
+        cin >> a;
+        max_val = max(max_val, a);
+    }
+    
+    long long ans = 2LL * n - 2LL * max_val - k + 1LL;
+    
+    cout << ans << "\n";
+}
+
+int main() {
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
