a42e79c658161b1455c65049927e5629396dad28
tiwariankit1708 <6295801ankit@gmail.com>
2026-08-29 16:42:38 +0530

    Add 2253A.cpp

---
+++ 2253A.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    if (isPrime(n + 1)) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    
+    return 0;
+}
