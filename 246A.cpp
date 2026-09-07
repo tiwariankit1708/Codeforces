3526485ebc1b119842a0124f6dfcc0fe46ca88ac
tiwariankit1708 <6295801ankit@gmail.com>
2026-09-07 19:58:37 +0530

    246A done

---
+++ 246A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    if (n <= 2) {
+        cout << -1 << endl;
+    } else {
+        for (int i = n; i >= 1; --i) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
